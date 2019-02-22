f6eb50978bdbf9f8a05a6c9a11421f0931221bf1
r-sierra <roberto@ideadigital.com.ar>
2019-02-22 14:23:16 -0300

    Set ActiveRecord::Migration target version as directly inheriting is no longer supported

--- db/migrate/20130717125312_add_index_on_active_on_spree_bank.rb
+++ db/migrate/20130717125312_add_index_on_active_on_spree_bank.rb
@@ -1,4 +1,4 @@
-class AddIndexOnActiveOnSpreeBank < ActiveRecord::Migration
+class AddIndexOnActiveOnSpreeBank < ActiveRecord::Migration[5.2]
   def change
     add_index :spree_banks, :active
   end
